a32717e7d6fdf82c12bd51d92dbfc02627343894
Buntu William <williambuntu1@gmail.com>
2023-03-26 18:01:43 +0200

    animated marks & scroll

--- app.js
+++ app.js
@@ -6,16 +6,29 @@ form.addEventListener('submit', e =>{
     e.preventDefault()
      let score = 0
      const userAnswers =[form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value,]
-
+ // check answers
      userAnswers.forEach((answer,index) =>{
 
         if ( answer === correctAnswer[index]){
-            score += 25
+            score += 20
 
         }
         
      })
+     // show the result page
+     scrollTo(0,0)
      
-     result.querySelector('span').textContent=`${score}%`
      result.classList.remove('d-none')
+
+     let output = 0
+     const timer = setInterval(() => {
+        result.querySelector('span').textContent=`${output}%`
+         if (output === score){
+            clearInterval(timer)
+         } else {
+            output++
+         }
+     }, 100);
 })
+// window object (global object)
+  
